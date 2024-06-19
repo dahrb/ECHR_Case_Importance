2d08d6ab5562fc1a85de0dbc1380024e95a08901
dahrb <d.bareham@liverpool.ac.uk>
2024-06-19 18:11:29 +0100

    implemented code for first validation experiment

--- gpt_count_tokens.py
+++ gpt_count_tokens.py
@@ -1,13 +1,20 @@
+"""
+Version history
+v1_1 = provides estimate for JSONL file required for batch api
+v1_0 = provides estimate for gpt-4 costs from the dataset under certain assumptions
+"""
+
+
 import pandas as pd
 import tiktoken
+import os
 
-
-#Strating point for code is this guide - https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+#Starting point for code is this guide - https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
 def num_tokens_from_string(data, zero_shot = True) -> int:
     """Returns the number of tokens in a text string."""
 
-    subject_matter = data['subject_matter'].values
-    questions = data['questions'].values
+    subject_matter = data['Subject Matter'].values
+    questions = data['Questions'].values
 
     gpt_4 = tiktoken.encoding_for_model('gpt-4')
     gpt_4_o = tiktoken.encoding_for_model('gpt-4o')
@@ -35,6 +42,7 @@ def num_tokens_from_string(data, zero_shot = True) -> int:
                 num_tokens += 3000
         
         output_tokens = 500*len(subject_matter)
+        print(output_tokens,'and',len(subject_matter))
 
         #calc cost
         if embed == 'gpt_4':
@@ -60,9 +68,48 @@ def num_tokens_from_string(data, zero_shot = True) -> int:
         print(f'Total Cost {embed}; zero-shot {zero_shot}: ${(cost_input + cost_output)/2:.2f} and £{((cost_input + cost_output)/0.78)/2:.2f}')
         print(f'Ablation Total cost ${ablation_cost/2:.2f} and £{(ablation_cost/0.78)/2:.2f}')
 
+def num_tokens_from_jsonl(data,name,output_tokens = 15):
+    ''''
+    Gives price estimation of JSONL file for batch API
+    '''
+    total_tokens = 0
+    tokens_per_message = 3
+    tokens_per_name = 1
+    gpt_4_o = tiktoken.encoding_for_model('gpt-4o')
+
+    for i in data['body']:
+        message = i['messages']
+        total_tokens += tokens_per_message
+        
+        for key, value in message[0].items():
+            total_tokens += len(gpt_4_o.encode(value))
+            if key == "name":
+                total_tokens += tokens_per_name
+        total_tokens += 3 
+    
+    cost_input =  (total_tokens/1_000_000) * 5 
+
+    output_tokens = output_tokens*len(data)
+    cost_output = (output_tokens/1_000_000) * 15
+
+    print(f'{name}:\n')
+    print(f"Number of input tokens in the text: {total_tokens}; estimated cost: ${cost_input/2:.2f} and £{(cost_input/0.78)/2:.2f}")
+    print(f"Number of output tokens (estimate): {output_tokens}; estimated cost: ${cost_output/2:.2f} and £{(cost_output/0.78)/2:.2f}")
+    
 if __name__ == '__main__':
 
-    data = pd.read_pickle('Data_1_0.pkl')
+    #num_tokens_from_string EXAMPLE
+    #data = pd.read_pickle('valid_data.pkl')
+    #print(data)
+    #num_tokens_from_string(data)
+    #num_tokens_from_string(data,zero_shot=False)
 
-    num_tokens_from_string(data)
-    num_tokens_from_string(data,zero_shot=False)
+    #num_tokens_from_jsonl EXAMPLE
+    for i in os.listdir('./Batches'):
+        if i.endswith('.jsonl'):
+            data = pd.read_json(f'./Batches/{i}',lines=True)
+            num_tokens_from_jsonl(data,name=i,output_tokens=15)
+            break
+    
+
+
